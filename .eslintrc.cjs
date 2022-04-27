a55e4d39bfc4197ca9239a96f33fe3aaa2f2d4ec
Jack Steam <jacksteamdev@gmail.com>
2022-04-27 14:48:10 -0500

    chore: add build tools

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,24 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended'],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 2022,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      plugins: ['@typescript-eslint'],
+    },
+  ],
   rules: {},
 }
---
+++ rollup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'rollup'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import esbuild from 'rollup-plugin-esbuild'
+
+export default defineConfig({
+  input: 'src/main.ts',
+  output: {
+    file: 'bin/migrate.js',
+    banner: '#!/usr/bin/env node',
+  },
+  plugins: [commonjs(), resolve(), esbuild({ target: 'esnext' })],
+})
